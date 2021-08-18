83bb94bf41d55024466a57a22b10babede9aa968
chetanrevappa <dgrchetan@gmail.com>
2021-08-18 23:26:28 +1000

    updated readme

--- db/SQLiteCreateTable.php
+++ db/SQLiteCreateTable.php
@@ -17,16 +17,14 @@ class SQLiteCreateTable {
         function to create the table if it not exists
     */
     public function createTables() {
-        $commands = ['CREATE TABLE IF NOT EXISTS tasks (
+        $command = 'CREATE TABLE IF NOT EXISTS tasks (
                         task_number VARCHAR(10) PRIMARY KEY,
                         task_name   VARCHAR (255) NOT NULL,
                         task_status VARCHAR (30),
                         task_priority VARCHAR (30)
-                      )'];
-        // execute the sql commands to create new tables
-        foreach ($commands as $command) {
-            $this->pdo->exec($command);
-        }
+                      )';
+        // execute the sql command to create new table
+        $this->pdo->exec($command);
     }
 
 }
